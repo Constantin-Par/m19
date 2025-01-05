2afc88bbbe7d9b94b631a3edae9d3a131d3d9b40
Константин <constantin.parshukov@yandex.ru>
2025-01-05 20:36:29 +0300

    2024/03/14 00:00|Домашнее задание по теме "QuerySet запросы в базу данных"

--- hw19/task1/models.py
+++ hw19/task1/models.py
@@ -22,3 +22,22 @@ class Game(models.Model):
 
     def __str__(self):
         return self.title
+
+
+'''
+Список QuerySet запросов в порядке вызовов, которые использовались для внесения изменений в БД
+from task1.models import Game
+Game.objects.create(title='Game_1 without age_limited', cost=1500, size=234132, description='description for game_1  without age_limited', age_limited=False)
+Game.objects.create(title='Game_2 with age_limited', cost=2100, size=344522, description='description for game_2  with age_limited', age_limited=True)
+Game.objects.create(title='Game_3 with age_limited', cost=2120, size=574753, description='description for game_3 with age_limited', age_limited=True)
+from task1.models import Buyer
+Buyer.objects.create(name='Buyer 22 yo', balance=1500, age=22)
+Buyer.objects.create(name='Buyer 30 yo', balance=2500, age=30)
+Buyer.objects.create(name='Buyer 17 yo', balance=1100, age=17)
+buyer_1 = Buyer.objects.get(id=1)
+buyer_2 = Buyer.objects.get(id=2)
+buyer_3 = Buyer.objects.get(id=3)
+Game.objects.get(id=1).buyer.set((buyer_1, buyer_2, buyer_3))
+Game.objects.get(id=2).buyer.set((buyer_1, buyer_2))
+Game.objects.get(id=3).buyer.set((buyer_1, ))
+'''
